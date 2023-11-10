79b1cd3c9184f3df4d64fad80123a38a331c08b8
Chandan Patnaik <120362748+chandan2013@users.noreply.github.com>
2023-11-10 16:21:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WordCounter from './Wordcounter';
+
+function App() {
+  return (
+    <div style={{marginTop:"200px", paddingRight:"100px", marginLeft:"450px", paddingLeft:"100px",border:"2px solid grey",width:"30%",height:"50%"}}>
+      <h1>Word Counter</h1>
+      <WordCounter />
+    </div>
+  );
+}
+
+export default App;
